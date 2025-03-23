2f157d0a24c6eab452dda366c847889dfba5e203
Muz1058 <muzaffarali3633@gmail.com>
2025-03-23 22:14:37 +0500

    compter using oop concept associations

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+
+
+
+
 
 public class Main {
     public static void main(String[] args) {
-
-        Connection con= DriverManager.getConnection();
         System.out.println("Hello world!");
     }
 }
